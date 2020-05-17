95fcf3e01f38dda696c1fb10d45da763abdeff2d
teddynanta <teddy.nanta99@gmail.com>
2020-05-17 11:29:23 +0700

    initial commit, learn mvc, controller

---
+++ app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php
+
+class About
+{
+    public function index($name, $crush)
+    {
+        echo "halo, aing $name, aing cinta $crush";
+    }
+    public function me()
+    {
+        echo "about/me";
+    }
+}
